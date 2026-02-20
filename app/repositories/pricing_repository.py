3c722c9865d28339361c0e09a83e93f9661fb534
Ritesh <riteshparmar17@gmail.com>
2026-02-20 01:32:27 -0500

    Implement pricing repository for MongoDB access

---
+++ app/repositories/pricing_repository.py
@@ -0,0 +1,30 @@
+from pymongo.collection import Collection
+from app.models.pricing_model import Pricing
+
+class PricingRepository:
+    def __init__(self, collection: Collection):
+        self.collection = collection
+
+    def get_by_product_id(self, product_id: str) -> Pricing | None:
+        data = self.collection.find_one({"product_id": product_id})
+
+        if not data:
+            return None
+        
+        return Pricing(
+            product_id=data["product_id"],
+            base_price=data["base_price"],
+            discount=data.get("discount", 0.0)
+        )
+    
+    def save(self, pricing: Pricing) -> None:
+        self.collection.update_one(
+            {"product_id": pricing.product_id},
+            {
+                "$set":{
+                    "base_price": pricing.base_price,
+                    "discount": pricing.discount
+                }
+            },
+            upsert=True
+        )
